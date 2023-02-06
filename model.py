ce33937e084842a9b5eabfe0b0d79030b5b7a59b
inhopp <inhonanananna@gmail.com>
2023-02-06 23:37:28 +0900

    update model.py

--- model.py
+++ model.py
@@ -1,57 +1,64 @@
 import torch
 import torch.nn as nn
-import numpy as np
 
 class Generator(nn.Module):
-    def __init__(self, channels=1, img_size=32, n_classes = 10):
-        super(Generator, self).__init__()
-        self.img_shape = (channels, img_size, img_size)
+    def __init__(self, channels=1, img_size=32, n_classes=10):
+        super(Generator, self).__init__()        
         self.label_emb = nn.Embedding(n_classes, n_classes)
+        self.init_size = img_size // 4
+        self.l1 = nn.Sequential(nn.Linear(100, 128 * self.init_size ** 2 ))
 
-        def block(input_dim, output_dim, normalize=True):
-            layers = [nn.Linear(input_dim, output_dim)]
-
-            if normalize:
-                layers.append(nn.BatchNorm1d(output_dim, 0.8))
-
-            layers.append(nn.LeakyReLU(0.2, inplace=True))
-
-            return layers
-        
-        self.model = nn.Sequential(
-            *block(100 + n_classes, 128, normalize=False),
-            *block(128, 256),
-            *block(256, 512),
-            *block(512, 1024),
-            nn.Linear(1024, int(np.prod(self.img_shape))),
+        self.conv_blocks = nn.Sequential(
+            nn.BatchNorm2d(128),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(128, 128, 3, stride=1, padding=1),
+            nn.BatchNorm2d(128, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(128, 64, 3, stride=1, padding=1),
+            nn.BatchNorm2d(64, 0.8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(64, channels, 3, stride=1, padding=1),
             nn.Tanh(),
         )
 
+
     def forward(self, z, labels):
-        # concatenate label embedding and image to produce input
         gen_input = torch.cat((self.label_emb(labels), z), -1)
-        img = self.model(gen_input)
-        img = img.view(img.size(0), *self.img_shape) # (batch_size, img_shape...)
+        out = self.l1(gen_input)
+        out = out.view(out.shape[0], 128, self.init_size, self.init_size)
+        img = self.conv_blocks(out) 
         return img
 
 
 class Discriminator(nn.Module):
     def __init__(self, channels=1, img_size=32, n_classes=10):
         super(Discriminator, self).__init__()
-        self.img_shape = (channels, img_size, img_size)
-        self.label_embedding = nn.Embedding(n_classes, n_classes)
+
+        def discriminator_block(in_filter, out_filter, bn=True):
+            block = [nn.Conv2d(in_filter, out_filter, 3, 2, 1), nn.LeakyReLU(0.2, inplace=True), nn.Dropout2d(0.25)]
+
+            if bn:
+                block.append(nn.BatchNorm2d(out_filter, 0.8))
+            
+            return block
 
         self.model = nn.Sequential(
-            nn.Linear(n_classes + int(np.prod(self.img_shape)), 512),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(512, 256),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(256, 1),
-            nn.Sigmoid(),
+            *discriminator_block(channels, 16, bn=False),
+            *discriminator_block(16, 32),
+            *discriminator_block(32, 64),
+            *discriminator_block(64, 128),
         )
 
-    def forward(self, img, labels):
-        d_in = torch.cat((img.view(img.size(0), -1), self.label_embedding(labels)), -1)
-        validity = self.model(d_in)
+        ds_size = img_size // 2 ** 4 # downsampled image size
 
-        return validity
+        self.adv_layer = nn.Sequential(nn.Linear(128 * ds_size ** 2, 1), nn.Sigmoid())
+        self.aux_layer = nn.Sequential(nn.Linear(128 * ds_size ** 2, n_classes), nn.Softmax())
+
+    def forward(self, img):
+        out = self.model(img)
+        out = out.view(out.shape[0], -1)
+        validity = self.adv_layer(out)
+        label = self.aux_layer(out)
+
+        return validity, label
